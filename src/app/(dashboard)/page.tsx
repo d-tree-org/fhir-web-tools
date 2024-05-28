40eca84380c24bee0cc9c7c18c206cc02fb4e0ee
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-05-28 11:49:12 +0200

    update location

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaAngleRight } from "react-icons/fa";
 
 const actions = [
   {
@@ -21,13 +22,16 @@ export default async function Page() {
           alignItems: "center",
           height: "70vh",
         }}
+        className="gap-4"
       >
         {actions.map((action) => (
           <Link key={action.link} href={action.link}>
             <div className="card w-96 bg-base-100 shadow-xl">
               <div className="card-body">
                 <h2 className="card-title">{action.name}</h2>
-                <div className="card-actions justify-end">{">"}</div>
+                <div className="card-actions justify-end">
+                  <FaAngleRight />
+                </div>
               </div>
             </div>
           </Link>
