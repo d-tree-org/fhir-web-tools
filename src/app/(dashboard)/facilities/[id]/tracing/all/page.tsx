07a710098310cbb0df1249e8a9feb0d3b8f17d9b
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-11-18 07:09:04 +0200

    init

--- src/app/(dashboard)/facilities/[id]/tracing/all/page.tsx
+++ src/app/(dashboard)/facilities/[id]/tracing/all/page.tsx
@@ -1,3 +1,22 @@
+import { columns, Payment } from "./components/columns";
+import { DataTable } from "./components/data-table";
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
+}
+
 export default async function Page() {
-  return <>Did you expect to find something, sucks to be you</>;
+  const data = await getData();
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
